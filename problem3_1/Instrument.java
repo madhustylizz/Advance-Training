0fe490e44342725bfdf251ee3efeb30259294233
madhustylizz <madhuboya466@gmail.com>
2022-04-29 19:02:36 +0530

    advance Training day1 and day2

--- problem3_1/Instrument.java
+++
@@ -1,5 +0,0 @@
-package problem3_1;
-public abstract class Instrument
-{
-public abstract void Play();
-}
--- problem7_2/AgeNotWithinRangeException.java
+++
@@ -1,9 +0,0 @@
-package problem7_2;
-
-class AgeNotWithInRangeException extends Exception
-{
-     public String toString()
-     {
-          return ("Age is not between 15 and 21. please ReEnter the Age");
-     }
-}
